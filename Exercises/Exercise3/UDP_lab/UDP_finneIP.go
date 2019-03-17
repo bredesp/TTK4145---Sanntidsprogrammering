248d6c3606c0d21a947b64debbb88b18c0854fa9
Simon Kvammen <43064821+simonkvammen@users.noreply.github.com>
2019-03-17 10:06:16 +0100

    Update UDP_finneIP.go

--- Exercises/Exercise3/UDP_lab/UDP_finneIP.go
+++ Exercises/Exercise3/UDP_lab/UDP_finneIP.go
@@ -14,7 +14,7 @@ func main() {
 	defer ServerConn.Close()
 
 	buf := make([]byte, 1024)
-
+	//c := exec.Command("cmd", "/C", "go run", "D:\\Desktop\\NTNU\\Semester 2\\TTK4145 - Sanntidsprogrammering\\GitHub\\TTK4145---Sanntidsprogrammering\\Exercises\\Exercise3\\UDP_lab\\UDP_send.go")
 	for {
 		fmt.Println("Venter på meldinger:")
 		n, addr, err := ServerConn.ReadFromUDP(buf)
