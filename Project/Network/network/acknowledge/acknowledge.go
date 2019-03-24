aa1150e9831f65ef60627d08f4fdc328c33776d8
Simon Kvammen <43064821+simonkvammen@users.noreply.github.com>
2019-03-24 16:19:19 +0100

    Oppdatert acknowledge.go med bedre navn og kommentarer

--- Project/Network/network/acknowledge/acknowledge.go
+++ Project/Network/network/acknowledge/acknowledge.go
@@ -1,191 +1,199 @@
 package acknowledge
 
-/*This module aims to maximize the chances for any message sent over the network to be received of all peers.
-The mid layer for the communication of updatemessages and statusmessages. Every message is stored in a struct,
-and then resent up to 10 times if there are peers no ack has been received from. When all acks have been received
-or the message is sent 10 times, it is deleted from the struct. This module communicates with the other peers on the network
-in addition to the Network module.
+/*
+This module will send every message up to 10 times if it doesen't
+receive acknowledgement from all the peers on the network.
 */
 import (
+	"../../../Backup"
+	"../bcast"
+	"../peers"
+
 	"fmt"
 	"os"
 	"os/exec"
 	"sync"
 	"time"
-
-	"../../../Backup"
-	"../bcast"
-	"../peers"
 )
-
-//TODO: Fix maps
-
-/*---------------------Ack message struct---------------------
-Id: ID of the Elevator
-SeqNo: Sequence number of the UDP packet
-MessageType: Type of UDP message that was sent/received out of:
-			- 0: UpdateMessages
-			- 1: StatusMessages
--------------------------------------------------------------*/
-type AckMsg struct {
-	Id      	string
-	SeqNo   	int
-	MessageType 	int
-}
 
 type SentMessages struct {
 	UpdateMessages    map[int]backup.UpdateMessage
 	StatusMessages    map[int]backup.StatusStruct
-	NumberOfTimesSent map[int]int
-	NotRecFromPeer    map[int][]string //Acks not received from active peers per seq. no.
-}
-
-type AckStruct struct {
-	AckMessage AckMsg
-	AckTimer   *time.Timer
-}
-
-type UpdateMessageStruct struct {
-	Message backup.UpdateMessage
-	SeqNo   int
+	NumbTimesSent 		map[int]int
+	NotAckFromPeer    map[int][]string
 }
 
 type StatusMessageStruct struct {
-	Message backup.StatusStruct
-	SeqNo   int
+	Message 			backup.StatusStruct
+	SequenceNum   int
+}
+
+type UpdateMessageStruct struct {
+	Message 			backup.UpdateMessage
+	SequenceNum   int
+}
+
+type AcknowledgeMessage struct {
+	ID      			string
+	SequenceNum   int
+	MessageType 	int				// 0 = UpdateMessages, 1 = StatusMessages
+}
+
+type AcknowledgeStruct struct {
+	AcknowledgeMsg 				AcknowledgeMessage
+	AcknowledgeTimer   		*time.Timer
 }
 
 var ID string
 var PORT string
-var seqNo = 0
+
+var seqNumb = 0
 var _mtx sync.Mutex = sync.Mutex{}
+
+var ch_timeout_ack = make(chan AcknowledgeMessage)
+var ch_TX_update = make(chan UpdateMessageStruct)
+var ch_RX_update = make(chan UpdateMessageStruct)
+var ch_TX_state = make(chan StatusMessageStruct)
+var ch_RX_state = make(chan StatusMessageStruct)
+var ch_TX_ack = make(chan AcknowledgeMessage)
+var ch_RX_ack = make(chan AcknowledgeMessage)
+
 var updateMessageToSend UpdateMessageStruct
 var statusMessageToSend StatusMessageStruct
 var sentMessages = new(SentMessages)
 var peerlist peers.PeerUpdate
 
-var ch_TX_update = make(chan UpdateMessageStruct)
-var ch_TX_state = make(chan StatusMessageStruct)
-var ch_RX_update = make(chan UpdateMessageStruct)
-var ch_RX_state = make(chan StatusMessageStruct)
-var ch_TX_ack = make(chan AckMsg)
-var ch_RX_ack = make(chan AckMsg)
-var TimeoutAckChan = make(chan AckMsg)
-
-//Main ack-goroutine. Communicates with enclosing Network module and other peers on the network through the bcast submodule.
 func Ack(ch_new_update chan<- backup.UpdateMessage, ch_new_status chan<- backup.StatusStruct, ch_ack_peer_update <-chan peers.PeerUpdate) {
 	defer func() {
 		if r := recover(); r != nil {
-			fmt.Println(r, " ACK fatal panic, unable to recover. Rebooting...", "./main -init=false -port="+PORT, " -id="+ID)
+			fmt.Println(r, " Panic in Ack, did not manage to recover. Trying to reboot: ", "port="+PORT, "id="+ID)
 			err := exec.Command("gnome-terminal", "-x", "sh", "-c", "./main -init=false -port="+PORT+" -id="+ID).Run()
 			if err != nil {
-				fmt.Println("Unable to reboot process, crashing...")
+				fmt.Println("ERROR: Did not manage to reboot!")
 			}
 		}
 		os.Exit(0)
 	}()
 	sentMessages.UpdateMessages = make(map[int]backup.UpdateMessage)
 	sentMessages.StatusMessages = make(map[int]backup.StatusStruct)
-	sentMessages.NumberOfTimesSent = make(map[int]int)
-	sentMessages.NotRecFromPeer = make(map[int][]string)
+	sentMessages.NumbTimesSent = make(map[int]int)
+	sentMessages.NotAckFromPeer = make(map[int][]string)
 
-	// Start the transmitter/receiver pair on some port
-	// These functions can take any number of channels! It is also possible to
-	//  start multiple transmitters/receivers on the same port.
 	go bcast.Transmitter(16569, ch_TX_update, ch_TX_state, ch_TX_ack)
 	go bcast.Receiver(16569, ch_RX_update, ch_RX_state, ch_RX_ack)
-	//Main loop in Ack-module
+
 	for {
 		select {
-		case update := <-ch_RX_update:
-			ackMessage := AckMsg{
-				Id:      		ID,
-				SeqNo:   		update.SeqNo,
-				MessageType: 	0,
-			}
-			if update.Message.Elevator != ID {
-				ch_new_update <- update.Message
-			}
-			ch_TX_ack <- ackMessage
-
-		case status := <-ch_RX_state:
-			ackMessage := AckMsg{
-				Id:      		ID,
-				SeqNo:   		status.SeqNo,
+			// When received new state, send acknowledge to peers and send state to
+			// Network-module and then to Backup-module
+		case newStatus := <-ch_RX_state:
+			ackMessage := AcknowledgeMessage{
+				ID:      			ID,
+				SequenceNum:  newStatus.SequenceNum,
 				MessageType: 	1,
 			}
 			ch_TX_ack <- ackMessage
-			ch_new_status <- status.Message
-		case notReceivedAck := <-TimeoutAckChan:
-			switch notReceivedAck.MessageType {
-			case 0: //UpdateMessages
+			ch_new_status <- newStatus.Message
+
+			// When received new update, send acknowledge to peers and send
+			// update to Network-module and then to Backup-module
+		case newUpdate := <-ch_RX_update:
+			ackMessage := AcknowledgeMessage{
+				ID:      			ID,
+				SequenceNum:  newUpdate.SequenceNum,
+				MessageType: 	0,
+			}
+			if newUpdate.Message.Elevator != ID {
+				ch_new_update <- newUpdate.Message
+			}
+			ch_TX_ack <- ackMessage
+
+			// If 15 ms has gone by, we have not received all the neccesary acks
+			// and we tried to send the message less than 10 times,
+			// we will try to resend the same message, and update the number of times
+			// the message is sent. Restart the acknowledge timer for another 15 ms.
+			// If sent more than 10 times, or if we have received all neccesary acks
+			// ("exist" will be false), don't try to resend. We will find out
+			// by other means if one or more peers is lost.
+			// This is only to "assure" against packet loss.
+		case timeoutAck := <-ch_timeout_ack:
+			switch timeoutAck.MessageType {
+			case 0:
 				_mtx.Lock()
-				_, ok := sentMessages.UpdateMessages[notReceivedAck.SeqNo]
-				if ok && (sentMessages.NumberOfTimesSent[notReceivedAck.SeqNo] < 10) { //Resend if sent <10 times
-					updateMessageToSend.Message = sentMessages.UpdateMessages[notReceivedAck.SeqNo]
-					updateMessageToSend.SeqNo = notReceivedAck.SeqNo
+				_, exists := sentMessages.UpdateMessages[timeoutAck.SequenceNum]
+				if exists && (sentMessages.NumbTimesSent[timeoutAck.SequenceNum] < 10) {
+					updateMessageToSend.Message = sentMessages.UpdateMessages[timeoutAck.SequenceNum]
+					updateMessageToSend.SequenceNum = timeoutAck.SequenceNum
 					ch_TX_update <- updateMessageToSend
-					sentMessages.NumberOfTimesSent[notReceivedAck.SeqNo] += 1
-					newAckStruct := AckStruct{
-						AckMessage: AckMsg{
-							Id:      		notReceivedAck.Id,
-							SeqNo:   		notReceivedAck.SeqNo,
+					sentMessages.NumbTimesSent[timeoutAck.SequenceNum] += 1
+					ackStruct := AcknowledgeStruct{
+						AcknowledgeMsg: AcknowledgeMessage{
+							ID:      			timeoutAck.ID,
+							SequenceNum:  timeoutAck.SequenceNum,
 							MessageType: 	0,
 						},
-						AckTimer: time.NewTimer(15 * time.Millisecond),
+						AcknowledgeTimer: time.NewTimer(15*time.Millisecond),
 					}
-					go ackTimer(TimeoutAckChan, newAckStruct)
+					go AcknowledgeTimeout(ch_timeout_ack, ackStruct)
 				}
 				_mtx.Unlock()
-			case 1: //StatusMessages
+			case 1:
 				_mtx.Lock()
-				_, ok := sentMessages.StatusMessages[notReceivedAck.SeqNo]
-				if ok && (sentMessages.NumberOfTimesSent[notReceivedAck.SeqNo] < 10) {
-					statusMessageToSend.Message = sentMessages.StatusMessages[notReceivedAck.SeqNo]
-					statusMessageToSend.SeqNo = notReceivedAck.SeqNo
+				_, exists := sentMessages.StatusMessages[timeoutAck.SequenceNum]
+				if exists && (sentMessages.NumbTimesSent[timeoutAck.SequenceNum] < 10) {
+					statusMessageToSend.Message = sentMessages.StatusMessages[timeoutAck.SequenceNum]
+					statusMessageToSend.SequenceNum = timeoutAck.SequenceNum
 					ch_TX_state <- statusMessageToSend
-					sentMessages.NumberOfTimesSent[notReceivedAck.SeqNo] += 1
-					newAckStruct := AckStruct{
-						AckMessage: AckMsg{
-							Id:      		notReceivedAck.Id,
-							SeqNo:   		notReceivedAck.SeqNo,
+					sentMessages.NumbTimesSent[timeoutAck.SequenceNum] += 1
+					ackStruct := AcknowledgeStruct{
+						AcknowledgeMsg: AcknowledgeMessage{
+							ID:      			timeoutAck.ID,
+							SequenceNum:  timeoutAck.SequenceNum,
 							MessageType: 	1,
 						},
-						AckTimer: time.NewTimer(15 * time.Millisecond),
+						AcknowledgeTimer: time.NewTimer(15*time.Millisecond),
 					}
-					go ackTimer(TimeoutAckChan, newAckStruct)
+					go AcknowledgeTimeout(ch_timeout_ack, ackStruct)
 				}
 				_mtx.Unlock()
 			}
-		case recAck := <-ch_RX_ack:
+
+			// When we receive ack from peers, update info on who we have received ack from.
+			// If we have received acks from all the peers (len() == 0), empty the associated sentMesseges
+			// category (either UpdateMessages or StatusMesseges) for the given message number
+			// (SequenceNum)
+		case receivedAck := <-ch_RX_ack:
 			_mtx.Lock()
-			_, ok := sentMessages.NotRecFromPeer[recAck.SeqNo] //In case the SeqNo has been deleted unexpectedly
-			if ok {
-				ind := stringInSlice(recAck.Id, sentMessages.NotRecFromPeer[recAck.SeqNo])
-				if ind != -1 {
-					sentMessages.NotRecFromPeer[recAck.SeqNo] = removeFromSlice(sentMessages.NotRecFromPeer[recAck.SeqNo], ind)
-					if len(sentMessages.NotRecFromPeer[recAck.SeqNo]) == 0 {
-						delete(sentMessages.NotRecFromPeer, recAck.SeqNo)
-						delete(sentMessages.NumberOfTimesSent, recAck.SeqNo) //Delete from NumberOfTimesSent
-						switch recAck.MessageType {
-						case 0: //UpdateMessages
-							delete(sentMessages.UpdateMessages, recAck.SeqNo) //Delete from UpdateMessages
-						case 1: //StatusMessages
-							delete(sentMessages.StatusMessages, recAck.SeqNo) //Delete from StatusMessages
+			_, exists := sentMessages.NotAckFromPeer[receivedAck.SequenceNum]
+			if exists {
+				index := stringInSlice(receivedAck.ID, sentMessages.NotAckFromPeer[receivedAck.SequenceNum])
+				if index != -1 {
+					sentMessages.NotAckFromPeer[receivedAck.SequenceNum] = removeFromSlice(sentMessages.NotAckFromPeer[receivedAck.SequenceNum], index)
+					if len(sentMessages.NotAckFromPeer[receivedAck.SequenceNum]) == 0 {
+						delete(sentMessages.NotAckFromPeer, receivedAck.SequenceNum)
+						delete(sentMessages.NumbTimesSent, receivedAck.SequenceNum)
+						switch receivedAck.MessageType {
+						case 0:
+							delete(sentMessages.UpdateMessages, receivedAck.SequenceNum)
+						case 1:
+							delete(sentMessages.StatusMessages, receivedAck.SequenceNum)
 						}
 					}
 				}
 			}
 			_mtx.Unlock()
-			//Delete lost peer from NotRecFromPeer
-		case newpeerlist := <-ch_ack_peer_update:
+
+			// A new peer-overview has been received in Network-module, and is forwarded here.
+			// If we have lost a peer since last time, delete it from our list over peers we want to
+			// receive acknowledge from.
+		case newPeerList := <-ch_ack_peer_update:
 			_mtx.Lock()
-			peerlist = newpeerlist
+			peerlist = newPeerList
 			if peerlist.Lost != "" {
-				for seqNo, peers := range sentMessages.NotRecFromPeer {
-					ind := stringInSlice(peerlist.Lost, peers)
-					if ind != -1 {
-						sentMessages.NotRecFromPeer[seqNo] = removeFromSlice(sentMessages.NotRecFromPeer[seqNo], ind)
+				for seqNumb, peers := range sentMessages.NotAckFromPeer {
+					index := stringInSlice(peerlist.Lost, peers)
+					if index != -1 {
+						sentMessages.NotAckFromPeer[seqNumb] = removeFromSlice(sentMessages.NotAckFromPeer[seqNumb], index)
 					}
 				}
 			}
@@ -194,63 +202,67 @@ func Ack(ch_new_update chan<- backup.UpdateMessage, ch_new_status chan<- backup.
 	}
 }
 
-//Function for sending update messages over the network. Adds the messages to the sentMessages struct. Spawns an ack-timer to wait for acks on the message.
-func SendUpdate(update backup.UpdateMessage) {
+// Send update-message to the net. If there are other peers on the net,
+// start timer for acknowledgements.
+func SendUpdate(newUpdate backup.UpdateMessage) {
 	_mtx.Lock()
 	defer _mtx.Unlock()
-	seqNo += 1
-	updateMessageToSend.Message = update
-	updateMessageToSend.SeqNo = seqNo
+	seqNumb += 1
+	updateMessageToSend.Message = newUpdate
+	updateMessageToSend.SequenceNum = seqNumb
 	ch_TX_update <- updateMessageToSend
-	if len(peerlist.Peers) != 0 {
-		sentMessages.UpdateMessages[seqNo] = update
-		sentMessages.NumberOfTimesSent[seqNo] = 1
-		sentMessages.NotRecFromPeer[seqNo] = peerlist.Peers
 
-		newAckStruct := AckStruct{
-			AckMessage: AckMsg{
-				Id:      		ID,
-				SeqNo:   		seqNo,
+	if len(peerlist.Peers) != 0 {
+		sentMessages.UpdateMessages[seqNumb] = newUpdate
+		sentMessages.NumbTimesSent[seqNumb] = 1
+		sentMessages.NotAckFromPeer[seqNumb] = peerlist.Peers
+
+		ackStruct := AcknowledgeStruct{
+			AcknowledgeMsg: AcknowledgeMessage{
+				ID:      			ID,
+				SequenceNum:  seqNumb,
 				MessageType: 	0,
 			},
-			AckTimer: time.NewTimer(15 * time.Millisecond),
+			AcknowledgeTimer: time.NewTimer(15*time.Millisecond),
 		}
-		go ackTimer(TimeoutAckChan, newAckStruct)
+		go AcknowledgeTimeout(ch_timeout_ack, ackStruct)
 	}
 }
 
-//Function for sending status messages over the network. Adds the messages to the sentMessages struct. Spawns an ack-timer to wait for acks on the message.
-func SendStatus(statusUpdate backup.StatusStruct) {
+// Send status-message to the net. If there are other peers on the net,
+// start timer for acknowledgements.
+func SendStatus(newStatus backup.StatusStruct) {
 	_mtx.Lock()
 	defer _mtx.Unlock()
-	seqNo += 1
-	statusMessageToSend.Message = statusUpdate
-	statusMessageToSend.SeqNo = seqNo
+	seqNumb += 1
+	statusMessageToSend.Message = newStatus
+	statusMessageToSend.SequenceNum = seqNumb
 	ch_TX_state <- statusMessageToSend
 
 	if len(peerlist.Peers) != 0 {
-		sentMessages.StatusMessages[seqNo] = statusMessageToSend.Message
-		sentMessages.NumberOfTimesSent[seqNo] = 1
-		sentMessages.NotRecFromPeer[seqNo] = peerlist.Peers
+		sentMessages.StatusMessages[seqNumb] = statusMessageToSend.Message
+		sentMessages.NumbTimesSent[seqNumb] = 1
+		sentMessages.NotAckFromPeer[seqNumb] = peerlist.Peers
 
-		newAckStruct := AckStruct{
-			AckMessage: AckMsg{
-				Id:      		ID,
-				SeqNo:   		seqNo,
+		ackStruct := AcknowledgeStruct{
+			AcknowledgeMsg: AcknowledgeMessage{
+				ID:      			ID,
+				SequenceNum:  seqNumb,
 				MessageType: 	1,
 			},
-			AckTimer: time.NewTimer(15 * time.Millisecond),
+			AcknowledgeTimer: time.NewTimer(15*time.Millisecond),
 		}
-		go ackTimer(TimeoutAckChan, newAckStruct)
+		go AcknowledgeTimeout(ch_timeout_ack, ackStruct)
 	}
 }
 
-//Goroutine for waiting for ack messages. When the timer finishes, the ack-message is sent to the TimeoutAckChan.
-func ackTimer(TimeoutAckChan chan<- AckMsg, ackStruct AckStruct) {
+// If 15 ms has gone by, send the associated acknowledge-message and let
+// for-select above check if all the acknowledgements has been received or not.
+func AcknowledgeTimeout(ch_timeout_ack chan<- AcknowledgeMessage, ackStruct AcknowledgeStruct) {
 	for {
 		select {
-		case <-ackStruct.AckTimer.C:
-			TimeoutAckChan <- ackStruct.AckMessage
+		case <-ackStruct.AcknowledgeTimer.C:
+			ch_timeout_ack <- ackStruct.AcknowledgeMsg
 			return
 		}
 	}
