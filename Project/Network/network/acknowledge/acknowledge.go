1b1183b057e2384031becf260dd1db8c4f0c4590
Brede Paulsen <bredesp@stud.ntnu.no>
2019-03-24 16:24:41 +0100

    Updated acknowledge

--- Project/Network/network/acknowledge/acknowledge.go
+++ Project/Network/network/acknowledge/acknowledge.go
@@ -267,3 +267,19 @@ func AcknowledgeTimeout(ch_timeout_ack chan<- AcknowledgeMessage, ackStruct Ackn
 		}
 	}
 }
+
+//Looks for a string in an array and returns the index.
+func stringInSlice(a string, list []string) int {
+	for ind, b := range list {
+		if b == a {
+			return ind
+		}
+	}
+	return -1
+}
+
+//Removes a string from an array, does not care about sorting.
+func removeFromSlice(s []string, i int) []string {
+	s[len(s)-1], s[i] = s[i], s[len(s)-1]
+	return s[:len(s)-1]
+}
